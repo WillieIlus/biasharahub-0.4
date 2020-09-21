16862b72b147d7f94fc08f06034b4d59c5c49d38
Wilson Irungu <amazingace00@gmail.com>
2020-09-21 23:53:44 +0300

    changed wsgi file

--- biasharahub/biasharahub/wsgi.py
+++ biasharahub/biasharahub/wsgi.py
@@ -11,10 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# from biasharahub import MyWSGIApp
+#
+# application = MyWSGIApp()
+# application = WhiteNoise(application, root='/path/to/static/files')
+# application.add_files('/path/to/more/static/files', prefix='more-files/')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biasharahub.settings")
 
